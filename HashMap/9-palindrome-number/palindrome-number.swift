7c43547d226ae61b00af3b9d899e30a34d6253da
Gideon Rotich <giddyrotich433@gmail.com>
2024-12-07 12:22:13 +0300

    Time: 4 ms (68.46%) | Memory: 18.2 MB (6.84%) - LeetSync

--- HashMap/9-palindrome-number/palindrome-number.swift
+++ HashMap/9-palindrome-number/palindrome-number.swift
@@ -1,19 +1,13 @@
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-         if x < 0 || (x % 10 == 0 && x != 0) {
+
+     if x < 0 || (x % 10 == 0 && x != 0) {
             return false
         }
-        
-        var originalNumber = x
-        var reversedNumber = 0
-        
-        while originalNumber > reversedNumber {
-            let digit = originalNumber % 10
-            reversedNumber = reversedNumber * 10 + digit
-            originalNumber /= 10
-        }
-        
-        // The number is a palindrome if the original number equals the reversed number
-        // or if the original number equals the reversed number divided by 10 (for odd-length numbers)
-        return originalNumber == reversedNumber || originalNumber == reversedNumber / 10   }
+
+        let original = String(x)
+        let reversed = String(original.reversed())
+
+        return original == reversed
+    }
 }
