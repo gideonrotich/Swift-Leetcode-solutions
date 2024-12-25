482558c8fa9188c3ecb79d6a4e86d6b527e30e29
Gideon Rotich <giddyrotich433@gmail.com>
2024-12-25 20:57:27 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (90.36%) - LeetSync

--- HashMap/1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.swift
+++ HashMap/1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.swift
@@ -1,15 +1,12 @@
 class Solution {
     func checkIfPangram(_ sentence: String) -> Bool {
-        var set = Set<Character>()
+    let array = (UnicodeScalar("a").value...UnicodeScalar("z").value).map { String( UnicodeScalar($0)!) }
 
-        var original = [Character]()
-        for string in sentence{
-            
-            set.insert(string)
+        for i in 0..<array.count{
+            if !sentence.contains(array[i]){
+                return false
+            }
         }
-        if set.count == 26{
-            return true
-        }
-        return false
+        return true
     }
 }
